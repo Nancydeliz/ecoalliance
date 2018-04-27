d5a1384c8c89e237669ed32b607174f27b0c50be
Nancydeliz <nancydelizchile@gmail.com>
2018-04-27 16:39:11 -0300

    programacion de la barra de busqueda lista

--- src/js/_nav.js
+++ src/js/_nav.js
@@ -14,5 +14,20 @@
 			  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
 			});
 			
+			/* Codigo para que aparezca la barra de búsqueda y se oculte el estado anterior */
+			$(function(){
+				$(".nav-buscar").click(function(event){
+				  $("#nav-s-1").addClass("ocultar");
+				  $("#nav-s-2").removeClass("ocultar");
+			   });		
+			});	
+			
+			$(function(){
+				$("#close-buscar").click(function(event){
+				  $("#nav-s-2").addClass("ocultar");
+				  $("#nav-s-1").removeClass("ocultar");
+				  
+			   });		
+			});	
 			
 		});	
