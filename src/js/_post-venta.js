062d0d8f16dcf855db820137623d9cd19d139a03
Nancydeliz <nancydelizchile@gmail.com>
2018-05-08 13:26:51 -0300

    Listo los JS para el nav-aux y su JS en el servicio post-venta

--- src/js/_post-venta.js
+++ src/js/_post-venta.js
@@ -4,17 +4,40 @@ $(document).ready(function(){
 	
 	/* Para que se oculte el menú de post-venta cuando se esté cercano al footer */	
 	$(function(){
-				$(window).scroll(function(){
-				if ($(window).scrollTop() > 3300)
-				{
-					$("#nav-aux-post-venta").fadeOut();
+		$(window).scroll(function(){
+			if ($(window).scrollTop() > 4000)
+			{
+				$("#nav-aux-post-venta").fadeOut();
 					
-				}
-				else
-				{
-					$("#nav-aux-post-venta").fadeIn();
+			}
+			else
+			{
+				$("#nav-aux-post-venta").fadeIn();
 					
-				}
-				});
+			}
+		});
+	});
+	
+	/* Para que al pasar el mouse por la sección se active el botón del menú auxiliar */		
+	$(function(){
+			$('#Descripcion1, #Descripcion2, #Descripcion3').mouseover(function(){
+				$('#nav-aux-post-venta a.active').removeClass('active');
+				$('a.link-naux-1').addClass('active');
 			});
-});
+		
+			$('#Servicios').mouseover(function(){
+				$('#nav-aux-post-venta a.active').removeClass('active');
+				$('a.link-naux-2').addClass('active');
+			});
+			
+			$('.FAQ').mouseover(function(){
+				$('#nav-aux-post-venta a.active').removeClass('active');
+				$('a.link-naux-3').addClass('active');
+			});
+			
+	});
+	
+	
+	
+	
+});//cierre de todo el código
