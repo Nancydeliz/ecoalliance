61c49ac12d7fe5ef3bcf55ef70c9353795d29c07
Nancydeliz <nancydelizchile@gmail.com>
2018-05-15 15:20:29 -0400

    pronabdo el plugins OnePage, aun sin lograr que aparezca el footer sin problemas

--- src/js/_activa_FullPage.js
+++ src/js/_activa_FullPage.js
@@ -1,4 +1,21 @@
-// JavaScript Document
-$(document).ready(function() {
-    $('#fullpage').fullpage();
+jQuery(document).ready(function() {
+		   jQuery('#fullpage').fullpage({
+				 verticalCentered: true,
+				resize: true,
+				scrollingSpeed: 700,
+				easing: 'easeInQuart',
+				menu: false,
+				navigation: true,
+				navigationPosition: 'right',
+				loopBottom: false,
+				loopTop: false,
+				autoScrolling: true,
+			    scrollBar: true,
+				css3: false,
+				paddingTop: 0,
+				paddingBottom: 0,
+			    fixedElements: '#header, .footer, .FAQ',
+			    continuousVertical: true,
+			   
+		   });
 });
