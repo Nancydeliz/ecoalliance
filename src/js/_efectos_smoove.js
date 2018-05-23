d7f72b5b0342f74b3e1a5287b824aede122bd265
Nancydeliz <nancydelizchile@gmail.com>
2018-05-23 10:16:14 -0400

    aun probando como colocar los efectos

--- src/js/_efectos_smoove.js
+++ src/js/_efectos_smoove.js
@@ -2,9 +2,40 @@
 
 $(document).ready(function(){
 	
-		$('.active .efecto').smoove({
-    		offset:'40%'
-    	});
+	function explode(){
+	  $('#watchme').addClass('animated title-section slideInUp count');
+	}
+	
+	setTimeout(explode, 3000);
+
+	
+	
+/*	if ($("#section2").hasClass('active')) {
+		
+		var e = document.getElementById("watchme");
+	
+		e.addEventListener("animationstart", listener, false);
+		e.addEventListener("animationend", listener, false);
+		e.addEventListener("animationiteration", listener, false);
+
+		e.className = "animated title-section slideInUp count";
+		function listener(e) {
+			  var l;
+			  switch(e.type) {
+				case "animationstart":
+				  l = "Iniciado: tiempo transcurrido " + e.elapsedTime;
+				  break;
+				case "animationend":
+				  l = "Finalizado: tiempo transcurrido " + e.elapsedTime;
+				  break;
+				case "animationiteration":
+				  l = "Nueva iteración comenzó a los " + e.elapsedTime;
+				  break;
+			  }
+				console.log(l);
+			}
+	}  */
+	
 		
 	
 });//cierre de todo el código
